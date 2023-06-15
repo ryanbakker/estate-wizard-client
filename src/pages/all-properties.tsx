d20ab7236452ee457765c555b47bfe01e506ed94
ryanbakkerNZ <ryan@ryanbakker.nz>
2023-06-15 18:08:55 +1200

    Pagination Layout

--- src/pages/all-properties.tsx
+++ src/pages/all-properties.tsx
@@ -1,6 +1,13 @@
 import { Add } from "@mui/icons-material";
 import { useTable } from "@pankod/refine-core";
-import { Box, Stack, Typography } from "@pankod/refine-mui";
+import {
+  Box,
+  Stack,
+  Typography,
+  TextField,
+  Select,
+  MenuItem,
+} from "@pankod/refine-mui";
 import { useNavigate } from "@pankod/refine-react-router-v6";
 
 import { PropertyCard, CustomButton } from "components";
@@ -10,6 +17,14 @@ const AllProperties = () => {
 
   const {
     tableQueryResult: { data, isLoading, isError },
+    current,
+    setCurrent,
+    setPageSize,
+    pageCount,
+    sorter,
+    setSorter,
+    filters,
+    setFilters,
   } = useTable();
 
   const allProperties = data?.data ?? [];
@@ -19,10 +34,65 @@ const AllProperties = () => {
 
   return (
     <Box>
+      <Box
+        mt="20px"
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: 3,
+        }}
+      >
+        <Stack direction="column" width="100%">
+          <Typography fontSize={25} fontWeight={700} color="#11142D">
+            {!allProperties.length
+              ? "There are no properties"
+              : "All Properties"}
+          </Typography>
+          <Box
+            mb={2}
+            mt={3}
+            display="flex"
+            width="84%"
+            justifyContent="space-between"
+            flexWrap="wrap"
+          >
+            <Box
+              display="flex"
+              gap={2}
+              flexWrap="wrap"
+              mb={{ xs: "20px", sm: 0 }}
+            >
+              <CustomButton
+                title={`Sort price`}
+                handleClick={() => {}}
+                backgroundColor="#475BE8"
+                color="#FCFCFC"
+              />
+              <TextField
+                variant="outlined"
+                color="info"
+                placeholder="Search by title"
+                value=""
+                onChange={() => {}}
+              />
+              <Select
+                variant="outlined"
+                color="info"
+                displayEmpty
+                required
+                inputProps={{ "aria-label": "Without label" }}
+                defaultValue=""
+                value=""
+                onChange={() => {}}
+              >
+                <MenuItem value="">All</MenuItem>
+              </Select>
+            </Box>
+          </Box>
+        </Stack>
+      </Box>
+
       <Stack direction="row" justifyContent="space-between" alignItems="center">
-        <Typography fontSize={25} fontWeight={700} color="#11142D">
-          All Properties
-        </Typography>
         <CustomButton
           title="Add Property"
           handleClick={() => navigate("/properties/create")}
@@ -51,6 +121,54 @@ const AllProperties = () => {
           />
         ))}
       </Box>
+
+      {/* Pagination */}
+
+      {allProperties.length > 0 && (
+        <Box display="flex" gap={4} mt={3} flexWrap="wrap">
+          <CustomButton
+            title="Previous"
+            handleClick={() => setCurrent((prev) => prev - 1)}
+            backgroundColor="#475BE8"
+            color="#FCFCFC"
+            disabled={!(current > 1)}
+          />
+          <Box
+            display={{ xs: "hidden", sm: "flex" }}
+            alignItems="center"
+            gap="5px"
+          >
+            Page{" "}
+            <strong>
+              {current} of {pageCount}
+            </strong>
+          </Box>
+          <CustomButton
+            title="Next"
+            handleClick={() => setCurrent((prev) => prev + 1)}
+            backgroundColor="#475BE8"
+            color="#FCFCFC"
+            disabled={current === pageCount}
+          />
+          <Select
+            variant="outlined"
+            color="info"
+            displayEmpty
+            required
+            inputProps={{ "aria-label": "Without label" }}
+            defaultValue={10}
+            value=""
+            onChange={() => {}}
+          >
+            {/* Show amount of properties */}
+            {[10, 20, 30, 40, 50].map((size) => (
+              <MenuItem key={size} value={size}>
+                Show {size}
+              </MenuItem>
+            ))}
+          </Select>
+        </Box>
+      )}
     </Box>
   );
 };
